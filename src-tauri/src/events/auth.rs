6ea6a9bdf0ca119cb768df0b08936d7f048b53d2
Runic <contact.runicdev@gmail.com>
2024-01-20 13:41:14 +0100

    Refactors registration logic

--- src-tauri/src/events/auth.rs
+++ src-tauri/src/events/auth.rs
@@ -1,42 +1,30 @@
-use sqlx::{Pool, Sqlite};
+use sqlx::{Acquire, Pool, Sqlite};
 use uuid::Uuid;
 
-use crate::models::{LoginRegisterAttempt, user::User};
+use crate::models::{user::User, LoginRegisterAttempt};
 
 pub async fn register_user(
     conn_pool: Pool<Sqlite>,
     register_attempt: LoginRegisterAttempt,
 ) -> Result<(), sqlx::Error> {
-    match conn_pool.begin().await {
-        Ok(mut tran) => {
-            let id = Uuid::new_v4();
+    let mut tran = conn_pool.begin().await?;
 
-            if let Err(err) = sqlx::query("INSERT INTO user (id, username) VALUES (?,?)")
-                .bind(id.clone().to_string())
-                .bind(register_attempt.username.clone())
-                .execute(&mut *tran)
-                .await
-            {
-                println!("Error inserting user into user table: {:?}", err);
-            }
-            if let Err(err) =
-                sqlx::query("INSERT INTO auth (id, username, password) VALUES (?,?,?)")
-                    .bind(Uuid::new_v4().to_string())
-                    .bind(register_attempt.username.clone())
-                    .bind(register_attempt.password)
-                    .execute(&mut *tran)
-                    .await
-            {
-                println!("Error inserting user into auth table: {:?}", err);
-            }
+    let id = Uuid::new_v4();
 
-            match tran.commit().await {
-                Ok(_) => Ok(()),
-                Err(err) => Err(err),
-            }
-        }
-        Err(err) => Err(err),
-    }
+    sqlx::query("INSERT INTO user (id, username) VALUES (?,?)")
+        .bind(id.clone().to_string())
+        .bind(register_attempt.username.clone())
+        .execute(&mut *tran)
+        .await?;
+
+    sqlx::query("INSERT INTO auth (id, username, password) VALUES (?,?,?)")
+        .bind(Uuid::new_v4().to_string())
+        .bind(register_attempt.username.clone())
+        .bind(register_attempt.password)
+        .execute(&mut *tran)
+        .await?;
+
+    tran.commit().await
 }
 
 pub async fn login_user(
@@ -47,7 +35,5 @@ pub async fn login_user(
         .bind(login_attempt.username)
         .bind(login_attempt.password)
         .fetch_one(&conn_pool)
-        .await 
+        .await
 }
-
-
