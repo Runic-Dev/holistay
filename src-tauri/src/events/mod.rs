40ab2d037041a31e10ec541b114f066810f7fb3e
Runic <contact.runicdev@gmail.com>
2024-02-28 20:38:53 +0100

    Refactors listen_to_frontend to be less verbose

--- src-tauri/src/events/mod.rs
+++ src-tauri/src/events/mod.rs
@@ -36,18 +36,73 @@ pub enum HolistayEvent {
 
 /// # Panics
 pub fn listen_to_frontend(app: &App, tx: Sender<HolistayEvent>) {
-    let tx_clone = tx.clone();
-    app.listen_global("register_attempt", move |event| {
-        let payload = event.payload().expect("Payload expected");
-        let register_attempt: LoginRegisterAttempt =
-            serde_json::from_str(payload).expect("Couldn't parse struct from payload");
-        let register_event = HolistayEvent::RegisterAttempt(register_attempt);
-        let tx_clone = tx_clone.clone();
+    handle_register_attempt(app, tx.clone());
+    handle_login_attempt(app, tx.clone());
+    handle_add_new_property(app, tx.clone());
+    handle_add_new_room_group(app, tx.clone());
+    handle_add_property(app, tx.clone());
+    handle_get_room_groups(app, tx.clone());
+    handle_get_property_data(app, tx);
+}
+
+fn handle_get_property_data(app: &App, tx: Sender<HolistayEvent>) {
+    app.listen_global("get_property_data", move |event| {
+        dbg!("event passed with property id: {}", &event);
+        let tx_clone = tx.clone();
+        //todo: payloads from svelte should have their own objects. no primitive obsessions here!
+        let property_id: String = serde_json::from_str(event.payload().expect("no payload found with property_data event")).expect("couldn't parse property id");
+        let property_data_req = HolistayEvent::PropertyDataRequested(property_id.to_string());
         tauri::async_runtime::spawn(async move {
-            let _ = tx_clone.send(register_event).await;
+            let _ = tx_clone.send(property_data_req).await;
         });
     });
-    let tx_clone = tx.clone();
+}
+
+fn handle_get_room_groups(app: &App, tx_clone: Sender<HolistayEvent>) {
+    app.listen_global("get_room_groups", move |event| {
+        let payload = event.payload().expect("No payload found for get room groups request");
+        let tx_clone = tx_clone.clone();
+        let get_room_groups_request: GetRoomGroupsRequest = serde_json::from_str(payload).expect("Couldn't parse NewPropertyRequest from payload");
+        tauri::async_runtime::spawn(async move {
+            let _ = tx_clone.send(HolistayEvent::GetRoomGroups(get_room_groups_request)).await;
+        });
+    });
+}
+
+fn handle_add_property(app: &App, tx_clone: Sender<HolistayEvent>) {
+    app.listen_global("get_properties", move |_event| {
+        let tx_clone = tx_clone.clone();
+        tauri::async_runtime::spawn(async move {
+            let _ = tx_clone.send(HolistayEvent::GetProperties).await;
+        });
+    });
+}
+
+fn handle_add_new_room_group(app: &App, tx_clone: Sender<HolistayEvent>) {
+    app.listen_global("add_new_room_group", move |event| {
+        let payload = event.payload().expect("No payload found for new room group request");
+        let new_room_group_request: NewRoomGroupRequest = serde_json::from_str(payload).expect("Couldn't parse NewRoomGroupRequest from payload");
+        let new_room_group_event = HolistayEvent::NewRoomGroup(new_room_group_request);
+        let tx_clone = tx_clone.clone();
+        tauri::async_runtime::spawn(async move {
+            let _ = tx_clone.send(new_room_group_event).await;
+        });
+    });
+}
+
+fn handle_add_new_property(app: &App, tx_clone: Sender<HolistayEvent>) {
+    app.listen_global("add_new_property", move |event| {
+        let payload = event.payload().expect("No payload found for new property request");
+        let new_property_request: NewPropertyRequest = serde_json::from_str(payload).expect("Couldn't parse NewPropertyRequest from payload");
+        let new_property_event = HolistayEvent::NewProperty(new_property_request);
+        let tx_clone = tx_clone.clone();
+        tauri::async_runtime::spawn(async move {
+            let _ = tx_clone.send(new_property_event).await;
+        });
+    });
+}
+
+fn handle_login_attempt(app: &App, tx_clone: Sender<HolistayEvent>) {
     app.listen_global("login_attempt", move |event| {
         let payload = event.payload().expect("Argh there's no bladdy payload");
         let login_attempt: LoginRegisterAttempt =
@@ -58,50 +113,17 @@ pub fn listen_to_frontend(app: &App, tx: Sender<HolistayEvent>) {
             let _ = tx_clone.send(login_event).await;
         });
     });
-    let tx_clone = tx.clone();
-    app.listen_global("add_new_property", move |event| {
-        let payload = event.payload().expect("No payload found for new property request");
-        let new_property_request: NewPropertyRequest = serde_json::from_str(payload).expect("Couldn't parse NewPropertyRequest from payload");
-        let new_property_event = HolistayEvent::NewProperty(new_property_request);
+}
+
+fn handle_register_attempt(app: &App, tx_clone: Sender<HolistayEvent>) {
+    app.listen_global("register_attempt", move |event| {
+        let payload = event.payload().expect("Payload expected");
+        let register_attempt: LoginRegisterAttempt =
+            serde_json::from_str(payload).expect("Couldn't parse struct from payload");
+        let register_event = HolistayEvent::RegisterAttempt(register_attempt);
         let tx_clone = tx_clone.clone();
         tauri::async_runtime::spawn(async move {
-            let _ = tx_clone.send(new_property_event).await;
-        });
-    });
-    let tx_clone = tx.clone();
-    app.listen_global("add_new_room_group", move |event| {
-        let payload = event.payload().expect("No payload found for new room group request");
-        let new_room_group_request: NewRoomGroupRequest = serde_json::from_str(payload).expect("Couldn't parse NewRoomGroupRequest from payload");
-        let new_room_group_event = HolistayEvent::NewRoomGroup(new_room_group_request);
-        let tx_clone = tx_clone.clone();
-        tauri::async_runtime::spawn(async move {
-            let _ = tx_clone.send(new_room_group_event).await;
-        });
-    });
-    let tx_clone = tx.clone();
-    app.listen_global("get_properties", move |_event| {
-        let tx_clone = tx_clone.clone();
-        tauri::async_runtime::spawn(async move {
-            let _ = tx_clone.send(HolistayEvent::GetProperties).await;
-        });
-    });
-    let tx_clone = tx.clone();
-    app.listen_global("get_room_groups", move |event| {
-        let payload = event.payload().expect("No payload found for get room groups request");
-        let tx_clone = tx_clone.clone();
-        let get_room_groups_request: GetRoomGroupsRequest = serde_json::from_str(payload).expect("Couldn't parse NewPropertyRequest from payload");
-        tauri::async_runtime::spawn(async move {
-            let _ = tx_clone.send(HolistayEvent::GetRoomGroups(get_room_groups_request)).await;
-        });
-    });
-    app.listen_global("get_property_data", move |event| {
-        dbg!("Event passed with property id: {}", &event);
-        let tx_clone = tx.clone();
-        //TODO: Payloads from Svelte should have their own objects. No primitive obsessions here!
-        let property_id: String = serde_json::from_str(event.payload().expect("No payload found with property_data event")).expect("Couldn't parse property ID");
-        let property_data_req = HolistayEvent::PropertyDataRequested(property_id.to_string());
-        tauri::async_runtime::spawn(async move {
-            let _ = tx_clone.send(property_data_req).await;
+            let _ = tx_clone.send(register_event).await;
         });
     });
 }
