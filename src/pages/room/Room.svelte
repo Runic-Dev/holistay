fc71bd1f0b57ab40055589672b0784ccde6b0475
Runic <contact.runicdev@gmail.com>
2024-05-03 13:18:36 +0200

    Refactors error handling in Room to try catch

--- src/pages/room/Room.svelte
+++ src/pages/room/Room.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import MainLayout from "@/MainLayout.svelte";
-  import {getContext, onDestroy, onMount} from "svelte";
+  import {getContext, onMount} from "svelte";
   import { propertyStore } from "@/store";
-  import { addBase64HtmlSyntax } from "@/utils";
+  import {addBase64HtmlSyntax} from "@/utils";
   import type { BackendHandler } from "@/backendHandlers";
   export let params: {
     roomId: string;
@@ -12,18 +12,15 @@
   $: room = null;
   getContext<BackendHandler>("backendHandler").foo();
   onMount(async () => {
-    // Having to do null / undefined checks everywhere due to weird JS behaviour
-    propertyStore.subscribe((ps) => {
-      if(ps) {
-        let property = ps.properties.find(p => p.id == params.propertyId);
-        if(property && property.roomGroups) {
-          let roomGroup = property.roomGroups.find(rg => rg.id == params.roomGroupId);
-          if(roomGroup && roomGroup.rooms) {
-            room = roomGroup.rooms.find(r => r.id == params.roomId);
-          }
-        }
-      }
-    });
+    try {
+      propertyStore.subscribe(ps => {
+        room = ps.properties.find(p => p.id == params.propertyId)
+                .roomGroups.find(rg => rg.id == params.roomGroupId)
+                .rooms.find(r => r.id == params.roomId)
+      });
+    } catch (e) {
+      console.log(`Room attempted onMount logic: ${e}`);
+    }
   });
 </script>
 
